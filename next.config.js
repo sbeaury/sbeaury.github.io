6a4e56ef56faf8af0337560e9f4c692e2e1b6d4c
Sebastien Beaury <sebastien.beaury@rezilio.com>
2021-06-19 18:23:04 -0400

    add next config

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const withPlugins = require("next-compose-plugins")
+const optimizedImages = require("next-optimized-images")
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+      imagesPublicPath: "/wallisconsultancy/_next/static/images/",
+    },
+  ],
+])

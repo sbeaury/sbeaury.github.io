5f34b0d88351250ee947b2e5e7dc75b2766d5bc5
Sebastien Beaury <sebastien.beaury@rezilio.com>
2021-06-25 17:42:03 -0400

    change google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,30 +2,30 @@ module.exports = {
   siteMetadata: {
     title: `Sebastien Beaury`,
     description: `Sebastien Beaury | Portfolio`,
-    author: `Sebastien Beaury | sbeaury@gmail.com`
+    author: `Sebastien Beaury | sbeaury@gmail.com`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-154687207-1"
-      }
+        trackingId: "UA-154687207-1",
+      },
     },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src`
-      }
+        path: `${__dirname}/src`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -39,29 +39,34 @@ module.exports = {
         start_url: `/`,
         theme_color: `#111`,
         display: `minimal-ui`,
-        include_favicon: false
-      }
+        include_favicon: false,
+      },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-webfonts`,
       options: {
-        fonts: [
-          {
-            family: `Bangers`,
-            subsets: [`latin`]
-          },
-          {
-            family: `Catamaran\:800`
-          },
-          {
-            family: `Roboto`
-          }
-        ]
-      }
-    }
+        fonts: {
+          google: [
+            {
+              family: `Bangers`,
+              variants: ["300", "400", "500"],
+              subsets: [`latin`],
+            },
+            {
+              family: `Catamaran`,
+              variants: ["300", "400", "500", "800"],
+            },
+            {
+              family: `Roboto`,
+              variants: ["300", "400", "500", "800"],
+            },
+          ],
+        },
+      },
+    },
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ]
-};
+  ],
+}
